2362f26d197a4785123cd8fa61c509bf16889563
unknown <raghul@thinkbigsoft.com>
2023-03-09 12:17:03 +0530

    first commit

---
+++ src/router/RootRouter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RouterProvider } from 'react-router'
+import { createBrowserRouter } from 'react-router-dom'
+
+const RootRouter = () => {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default RootRouter
